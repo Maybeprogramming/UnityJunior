d64e13ddd23795a28764ef54d3898381f6bab439
Maybeonce <a-u-rom@yandex.ru>
2024-04-11 10:31:52 +0300

    +

--- Assets/Tasks/Task_Genius_Of_Style_Code/Scripts/Movement.cs
+++ Assets/Tasks/Task_Genius_Of_Style_Code/Scripts/Movement.cs
@@ -5,23 +5,23 @@ public class Movement : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private Transform _waypoints;
 
-    private Transform[] _AllWaypoints;
+    private Transform[] _allWaypoints;
     private int _currentWaypointIndex;
 
     private void Start()
     {
         _currentWaypointIndex = 0;
-        _AllWaypoints = new Transform[_waypoints.childCount];
+        _allWaypoints = new Transform[_waypoints.childCount];
 
         for (int i = 0; i < _waypoints.childCount; i++)
         {
-            _AllWaypoints[i] = _waypoints.GetChild(i).GetComponent<Transform>();
+            _allWaypoints[i] = _waypoints.GetChild(i).GetComponent<Transform>();
         }
     }
 
     private void Update()
     {
-        var nextWaypoint = _AllWaypoints[_currentWaypointIndex];
+        var nextWaypoint = _allWaypoints[_currentWaypointIndex];
         transform.position = Vector3.MoveTowards(transform.position, nextWaypoint.position, _speed * Time.deltaTime);
 
         if (transform.position == nextWaypoint.position)
@@ -34,12 +34,12 @@ public class Movement : MonoBehaviour
     {
         _currentWaypointIndex++;
 
-        if (_currentWaypointIndex == _AllWaypoints.Length)
+        if (_currentWaypointIndex == _allWaypoints.Length)
         {
             _currentWaypointIndex = 0;
         }
 
-        var nextWaypoint = _AllWaypoints[_currentWaypointIndex].transform.position;
+        var nextWaypoint = _allWaypoints[_currentWaypointIndex].transform.position;
         transform.forward = nextWaypoint - transform.position;
 
         return nextWaypoint;
