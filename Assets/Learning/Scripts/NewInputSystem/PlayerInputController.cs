92f4d16cf25e45464be84edbcd9587c464d25c50
Maybeonce <a-u-rom@yandex.ru>
2024-04-19 15:28:39 +0300

    up

--- Assets/Learning/Scripts/NewInputSystem/PlayerInputController.cs
+++ Assets/Learning/Scripts/NewInputSystem/PlayerInputController.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerInputController : MonoBehaviour
 {
+    [SerializeField] private GameObject _prefabBullet;
+    [SerializeField] private float _bulletSpeed;
+    [SerializeField] private Vector3 _moveDirection;
+    [SerializeField] private float _playerSpeed;
+
     private PlayerInput _playerInput;
 
     private void Awake()
     {
         _playerInput = new PlayerInput();
-        _playerInput.Player.Shoot.performed += OnShooted;
+        _playerInput.Player.Shoot.performed += OnShoot;
     }
 
     private void OnShooted(InputAction.CallbackContext context)
@@ -37,6 +43,23 @@ public class PlayerInputController : MonoBehaviour
     //Вариант для режима InvokeUnityEvents
     public void OnShoot(InputAction.CallbackContext context)
     {
+        var bullet = Instantiate(_prefabBullet, transform.position + Vector3.forward * 2f, Quaternion.identity);
+        bullet.GetComponent<Rigidbody>().velocity = Vector3.forward * _bulletSpeed;
+
         Debug.Log("OnShoot");
+    }
+
+    private void Update()
+    {
+        var vector2direction = _playerInput.Player.Move.ReadValue<Vector2>();
+        _moveDirection = new Vector3(vector2direction.x, 0f, vector2direction.y);
+
+        transform.position += _moveDirection * _playerSpeed * Time.deltaTime;
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        var vector2direction = context.ReadValue<Vector2>();
+        _moveDirection = new Vector3(vector2direction.x, 0f,  vector2direction.y);
     }
 }
